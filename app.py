23ec5b9b6497c3ff226c3519b381d25001d0244b
Alexandre Le Begge <alexandre.lebegge@gmail.com>
2022-04-14 16:38:10 +0200

    improve streamlit

--- app.py
+++ app.py
@@ -1,29 +1,37 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import random
 from pathlib import Path
 import os
 
 @st.cache
 def df_from_csv(csv_file):
-    df = pd.read_csv(csv_file).sample(5000)
-    df['random'] = random.randint(0,3)
+    df = pd.read_csv(csv_file)
     return df
 
 data_load_state = st.text('Loading data...')
-st.text(os.path.abspath(''))
-# path = Path(r"data\v3.csv")
-df = df_from_csv(r"O:\Enron_Email_Dataset\data\v4.csv")
-# df = pd.read_csv("data\v4.csv").sample(15000)
+df = df_from_csv(r"data/streamlit_df.csv")
 data_load_state.text('Loading data...done!')
 
 st.title("Enron Study Case.")
 st.write(""" 
 This webpage allow you to iterate in all available mails from Enron case.
 """)
-author =  st.selectbox("Please select a person to investigate", list(df["x_origin"].unique()))
-cluster = st.selectbox("Main Topic",["0","1","2","3"])
+author =  st.sidebar.selectbox("Please select a person to investigate", list(df["x_origin"].unique()))
 df_author = df[df['x_origin'] == author]
-df_clustered = df_author[df_author['random'] == int(cluster)]
-st.dataframe(df_clustered[['random','x_origin','body']])
+# cluster = st.sidebar.selectbox("Main Topic",["0","1","2","3"])
+cluster = st.sidebar.selectbox("Main Topic",list(df_author['Dominant_Topic'].unique()))
+# cluster = st.selectbox()
+df_clustered = df_author[df_author['Dominant_Topic'] == int(cluster)]
+col1, col2 = st.columns(2)
+col1.metric("Number of total emails", df_author.shape[0])
+col2.metric(f"Period email in {cluster} cluster", df_clustered.shape[0])
+st.dataframe(df_clustered[['date','to','from','subject','body']])
+email = st.sidebar.selectbox("Select an email to view more", list(df_clustered['subject']))
+df_email = df_clustered[df_clustered['subject']==email]
+col3, col4, col5 = st.columns(3)
+col3.metric('Date', str(df_email['date']))
+col4.metric('From', str(df_email['from']))
+col5.metric('To', str(df_email['to']))
+# with st.expander("Click to expand the email"):
+st.text(df_email['body'].to_string())
---
+++ test.py
@@ -0,0 +1,4 @@
+import pandas as pd 
+
+df = pd.read_csv('data/streamlit_df.csv')
+print(df.head())
